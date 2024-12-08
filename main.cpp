901d01dab1c402aa016064b8bb80b855cb95f837
alexandra-gruia <128381596+alexandra-gruia@users.noreply.github.com>
2024-12-08 23:10:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,21 @@
 #include <cstring> //memcpy
 using namespace std;
 
+class Resurse {
+private:
+    char* resurse; // resursa gestionata
+    size_t dimensiune;  
+
+public:
+    // constructor 
+    Resurse(size_t dim = 0) : resurse(nullptr), dimensiune(dim)
+    {
+        if (dimensiune > 0) {
+            resurse = new char[dimensiune];
+            cout << "Resursa alocata.\n";
+    }
+}
+
 int main() {
   
 
